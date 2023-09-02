c046968ccb6ee066f1af5a4242f47b7f15988660
Beudicri <ys4512558@naver.com>
2023-09-02 15:46:20 +0900

    Level 18_2

--- 단계별로 풀어보기/19.재귀/Problem/src/Main.java
+++ 단계별로 풀어보기/19.재귀/Problem/src/Main.java
@@ -2,6 +2,28 @@ import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+
+    }
+
+    private static void prob10870() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+
+        int res = recur10870(N);
+        sb.append(res);
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+
+    public static int recur10870(int N){
+        if(N == 0 || N == 1){
+            return N;
+        }
+        return recur10870(N-1) + recur10870(N-2);
     }
 
     private static void prob27433() throws IOException {
