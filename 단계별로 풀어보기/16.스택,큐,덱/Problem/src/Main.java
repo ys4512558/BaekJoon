0a4a33ec938ee209bc97131200db57d92ae0f766
beudicri <ys4512558@naver.com>
2023-09-11 22:48:20 +0900

    Level 20_5

--- 단계별로 풀어보기/16.스택,큐,덱/Problem/src/Main.java
+++ 단계별로 풀어보기/16.스택,큐,덱/Problem/src/Main.java
@@ -3,7 +3,16 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
 
+        Deque<Integer> deque = new ArrayDeque<>();
+        for (int i = 0; i < M; i++) {
+            
+        }
     }
 
     private static void prob24511() throws IOException {
