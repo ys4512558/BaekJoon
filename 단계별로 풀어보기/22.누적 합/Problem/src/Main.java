2de7507adbe6b6a5e19356a609b9c9a761a5cf71
SeungCheol <ys4512558@naver.com>
2023-09-25 13:41:28 +0900

    Level 22_4

--- 단계별로 풀어보기/22.누적 합/Problem/src/Main.java
+++ 단계별로 풀어보기/22.누적 합/Problem/src/Main.java
@@ -8,6 +8,54 @@ public class Main {
 
     static int cnt[][];
     public static void main(String[] args) throws IOException {
+    }
+
+    private static void prob11660() throws IOException {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int arr[][] = new int[N][N];
+        long sum[][] = new long[N][N];
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+                if(i == 0 && j == 0)
+                    sum[i][j] = arr[i][j];
+                else if(j == 0)
+                    sum[i][j] = sum[i-1][j] + arr[i][j];
+                else if(i == 0)
+                    sum[i][j] = sum[i][j-1] + arr[i][j];
+                else
+                    sum[i][j] = sum[i][j-1] + sum[i-1][j] - sum[i-1][j-1] + arr[i][j];
+            }
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x1 = Integer.parseInt(st.nextToken())-1;
+            int y1 = Integer.parseInt(st.nextToken())-1;
+            int x2 = Integer.parseInt(st.nextToken())-1;
+            int y2 = Integer.parseInt(st.nextToken())-1;
+
+            long res = 0;
+            if(x1 == 0 && y1 == 0) //x1, y1이 0이면 sum[x2][y2]자체
+                res = sum[x2][y2];
+            else if(x1 == 0)  //x1 == 0이면, 열만 빼면 됨.
+                res = sum[x2][y2] - sum[x2][y1-1];
+            else if (y1 == 0) //y1 == 0이면, 행만 빼면 됨.
+                res = sum[x2][y2] - sum[x1-1][y2];
+            else
+                res = sum[x2][y2] - sum[x1-1][y2] - sum[x2][y1-1] + sum[x1-1][y1-1];
+
+            sb.append(res).append("\n");
+        }
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+
+    private static void prob16139() throws IOException {
         String str = br.readLine();
         cnt = new int[str.length()+1][26];
         for (int i = 1; i <= str.length(); i++) {
