7921c133d3c3becd971d9e4a5c0a536dedc34841
Beudicri <ys4512558@naver.com>
2023-10-02 15:43:45 +0900

    Level 21_16

--- 단계별로 풀어보기/21.동적계획법/Problem/src/Main.java
+++ 단계별로 풀어보기/21.동적계획법/Problem/src/Main.java
@@ -37,6 +37,39 @@ public class Main {
 
     }
 
+    private static void prob12865() throws IOException {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        int[] W = new int[N + 1];
+        int[] V = new int[N + 1];
+        int[][] dp = new int[N + 1][K + 1];
+
+        for (int i = 1; i <= N; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            W[i] = Integer.parseInt(st.nextToken());
+            V[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= K; j++) {
+                if(W[i] > j) {
+                    dp[i][j] = dp[i - 1][j];
+                }
+                else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - W[i]] + V[i]);
+                }
+
+            }
+        }
+        sb.append(dp[N][K]);
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+
     private static void prob9251() throws IOException {
         char str1[] = br.readLine().toCharArray();
         char str2[] = br.readLine().toCharArray();
