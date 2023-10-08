6a5a12469aec07a0a0327255da986c77b6d22304
Beudicri <ys4512558@naver.com>
2023-10-08 14:57:36 +0900

    prob30007

--- 기타/Problem/src/Main.java
+++ 기타/Problem/src/Main.java
@@ -7,6 +7,21 @@ public class Main {
     static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) throws IOException {
+        prob30007();
+    }
 
+    private static void prob30007() throws IOException {
+        int N = Integer.parseInt(br.readLine());
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+            int X = Integer.parseInt(st.nextToken());
+            int W = A*(X-1)+B;
+            sb.append(W).append("\n");
+        }
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
     }
 }
