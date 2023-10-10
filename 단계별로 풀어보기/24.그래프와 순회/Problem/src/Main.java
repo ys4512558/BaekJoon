ca7c2b361b859b5bbe7e052a5e4d29d19494fd15
SeungCheol <ys4512558@naver.com>
2023-10-10 11:23:32 +0900

    Level 24_6

--- 단계별로 풀어보기/24.그래프와 순회/Problem/src/Main.java
+++ 단계별로 풀어보기/24.그래프와 순회/Problem/src/Main.java
@@ -6,9 +6,151 @@ public class Main {
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     static StringBuilder sb = new StringBuilder();
     static int visit[];
+    static boolean visited[];
     static int cnt = 1;
+    static int res = 0;
     static Queue<Integer> queue;
+
     public static void main(String[] args) throws IOException {
+
+    }
+
+    private static void prob1260() throws IOException {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int V = Integer.parseInt(st.nextToken());
+        visited = new boolean[N+1];
+        Map<Integer, ArrayList<Integer>> edge = new HashMap<>();
+        for (int i = 0; i < N; i++) {
+            edge.put((i+1), new ArrayList<>());
+        }
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int node1 = Integer.parseInt(st.nextToken());
+            int node2 = Integer.parseInt(st.nextToken());
+            edge.get(node1).add(node2);
+            edge.get(node2).add(node1);
+        }
+        sb.append(V).append(" ");
+        dfs1260(edge, V);
+        sb.append("\n");
+
+        queue = new LinkedList<>();
+        visited = new boolean[N+1];
+        bfs1260(edge, V);
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+
+    private static void dfs1260(Map<Integer, ArrayList<Integer>> edge, int v) {
+        visited[v] = true;
+        Collections.sort(edge.get(v));
+        for (int i = 0; i < edge.get(v).size(); i++) {
+            if(!visited[edge.get(v).get(i)]) {
+                sb.append(edge.get(v).get(i)).append(" ");
+                dfs1260(edge, edge.get(v).get(i));
+            }
+        }
+    }
+    private static void bfs1260(Map<Integer, ArrayList<Integer>> edge, int v) {
+        queue.add(v);
+        visited[v] = true;
+        sb.append(v).append(" ");
+        while (!queue.isEmpty()) {
+            int q = queue.poll();
+            Collections.sort(edge.get(q));
+            for (int i = 0; i < edge.get(q).size(); i++) {
+                if(!visited[edge.get(q).get(i)]){
+                    visited[edge.get(q).get(i)] = true;
+                    queue.add(edge.get(q).get(i));
+                    sb.append(edge.get(q).get(i)).append(" ");
+                }
+            }
+        }
+    }
+
+    private static void prob2606_bfs() throws IOException {
+        int N = Integer.parseInt(br.readLine());
+        int K = Integer.parseInt(br.readLine());
+        visited = new boolean[N+1];
+        queue = new LinkedList<>();
+        Map<Integer, ArrayList<Integer>> edge = new HashMap<>();
+        for (int i = 0; i < N; i++) {
+            edge.put((i+1), new ArrayList<>());
+        }
+        for (int i = 0; i < K; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int node1 = Integer.parseInt(st.nextToken());
+            int node2 = Integer.parseInt(st.nextToken());
+            edge.get(node1).add(node2);
+            edge.get(node2).add(node1);
+        }
+        bfs2606(edge, 1);
+
+        int count = 0;
+        for (int i = 2; i <= N; i++) {
+            if(visited[i])
+                count++;
+        }
+        sb.append(count);
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+
+    private static void bfs2606(Map<Integer, ArrayList<Integer>> edge, int start) {
+        visited[start] = true;
+        queue.add(start);
+        while (!queue.isEmpty()){
+            int q = queue.poll();
+            for (int i = 0; i < edge.get(q).size(); i++) {
+                if(!visited[edge.get(q).get(i)]){
+                    visited[edge.get(q).get(i)] = true;
+                    queue.add(edge.get(q).get(i));
+                }
+            }
+        }
+    }
+
+    private static void prob2606_dfs() throws IOException {
+        int N = Integer.parseInt(br.readLine());
+        int K = Integer.parseInt(br.readLine());
+        visited = new boolean[N+1];
+        Map<Integer, ArrayList<Integer>> edge = new HashMap<>();
+        for (int i = 0; i < N; i++) {
+            edge.put((i+1), new ArrayList<>());
+        }
+        for (int i = 0; i < K; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int node1 = Integer.parseInt(st.nextToken());
+            int node2 = Integer.parseInt(st.nextToken());
+            edge.get(node1).add(node2);
+            edge.get(node2).add(node1);
+        }
+        dfs2606(edge, 1);
+        int count = 0;
+        for (int i = 2; i <= N; i++) {
+            if(visited[i])
+                count++;
+        }
+        sb.append(count);
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+
+    private static void dfs2606(Map<Integer, ArrayList<Integer>> edge, int start) {
+        visited[start] = true;
+        for (int i = 0; i < edge.get(start).size(); i++) {
+            if(!visited[edge.get(start).get(i)]){
+                dfs2606(edge, edge.get(start).get(i));
+            }
+        }
+    }
+
+    private static void prob24445() throws IOException {
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
@@ -34,6 +176,7 @@ public class Main {
         bw.flush();
         bw.close();
     }
+
     private static void bfs24445(Map<Integer, ArrayList<Integer>> edge, int r){
         visit[r] = cnt++;
         queue.add(r);
