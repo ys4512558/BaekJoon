a3ae473dabbac06a232ce12db6b74548f7db9c61
Beudicri <ys4512558@naver.com>
2023-10-18 23:19:19 +0900

    Level 22_prob11659

--- 단계별로 풀어보기/22.누적 합/Problem/src/Main.java
+++ 단계별로 풀어보기/22.누적 합/Problem/src/Main.java
@@ -8,12 +8,6 @@ public class Main {
 
     static int cnt[][];
     public static void main(String[] args) throws IOException {
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(st.nextToken());
-
-        int chess[][] = new int[N][M];
 
     }
 
