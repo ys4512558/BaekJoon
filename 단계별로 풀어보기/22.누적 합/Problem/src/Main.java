73adef2a918ff425b1c869e89e175717a8cbe913
Beudicri <ys4512558@naver.com>
2023-10-19 17:52:40 +0900

    Level 22_6

--- 단계별로 풀어보기/22.누적 합/Problem/src/Main.java
+++ 단계별로 풀어보기/22.누적 합/Problem/src/Main.java
@@ -10,6 +10,96 @@ public class Main {
 
     }
 
+    private static void prob11660_2() throws IOException {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int arr[][] = new int[N][N];
+        int sum[][] = new int[N+1][N+1];
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                sum[i][j] = sum[i][j - 1] + sum[i - 1][j] - sum[i - 1][j - 1] + arr[i - 1][j - 1];
+            }
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x1 = Integer.parseInt(st.nextToken());
+            int y1 = Integer.parseInt(st.nextToken());
+            int x2 = Integer.parseInt(st.nextToken());
+            int y2 = Integer.parseInt(st.nextToken());
+
+            int res = sum[x2][y2] - sum[x2][y1 - 1] - sum[x1 - 1][y2] + sum[x1 - 1][y1 - 1];
+            sb.append(res).append("\n");
+        }
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+
+    private static void prob16139_2() throws IOException {
+        String input = br.readLine();
+        int N = Integer.parseInt(br.readLine());
+
+        int sum[][] = new int[input.length()+1][26];
+        for (int i = 0; i < input.length(); i++) {
+            for (int j = 0; j < 26; j++) {
+                int idx = input.charAt(i) - 'a';
+                if (j == idx) {
+                    sum[i+1][j] = sum[i][j] + 1;
+                } else {
+                    sum[i+1][j] = sum[i][j];
+                }
+            }
+        }
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int idx = st.nextToken().charAt(0) - 'a';
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            int res = sum[end+1][idx] - sum[start][idx];
+            sb.append(res).append("\n");
+        }
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+
+    private static void prob2559_2() throws IOException {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        int arr[] = new int[N];
+        int sum[] = new int[N+1];
+
+        st = new StringTokenizer(br.readLine());
+        int tmp = 0;
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        sum[0] = 0;
+        for (int i = 1; i <= N; i++) {
+            sum[i] = sum[i - 1] + arr[i-1];
+        }
+
+        int max = -1000001;
+        for (int i = K; i < sum.length; i++) {
+            max = Math.max(max, sum[i] - sum[i - K]);
+        }
+        sb.append(max);
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+
     private static void prob25682() throws IOException {
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
