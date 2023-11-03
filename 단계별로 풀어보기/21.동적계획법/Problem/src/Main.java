c9a92d9245a2524b84b7c1db605bf0feedfa9e13
beudicri <ys4512558@naver.com>
2023-11-03 16:31:39 +0900

    SOLVED : DP11052

--- 단계별로 풀어보기/21.동적계획법/Problem/src/Main.java
+++ 단계별로 풀어보기/21.동적계획법/Problem/src/Main.java
@@ -30,6 +30,29 @@ public class Main {
 
     }
 
+    private static void prob11052() throws IOException {
+        int N = Integer.parseInt(br.readLine());
+        int arr[] = new int[1001];
+        int dp[] = new int[1001];
+
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+            dp[i] = arr[i];
+        }
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= i; j++) {
+                dp[i] = Math.max(dp[i], dp[i - j] + arr[j]);
+            }
+        }
+
+        sb.append(dp[N]);
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+
     private static void prob2193() throws IOException {
         int N = Integer.parseInt(br.readLine());
         long dp[] = new long[91];
