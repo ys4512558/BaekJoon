ba6d3fff89d4306478894e51e56badff773d0152
beudicri <ys4512558@naver.com>
2023-11-17 17:51:00 +0900

    SOLVING : DP11055

--- 단계별로 풀어보기/21.동적계획법/Problem/src/Main.java
+++ 단계별로 풀어보기/21.동적계획법/Problem/src/Main.java
@@ -27,6 +27,22 @@ public class Main {
     static int tile[];
     static long num[][];
     public static void main(String[] args) throws IOException {
+        int N = Integer.parseInt(br.readLine());
+
+        int arr[] = new int[N+1];
+        int dp[] = new int[N+1];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i+1] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 1; i < N; i++) {
+            if (arr[i] > arr[i - 1]) {
+                dp[i] = dp[i - 1] + arr[i];
+            }
+        }
+
 
     }
 
