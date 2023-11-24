c3013538ae56e181d01ed183c6e4237193ebe812
beudicri <ys4512558@naver.com>
2023-11-24 09:49:41 +0900

    SOLVED : PROB5585

--- 단계별로 풀어보기/23.그리디 알고리즘/Problem/src/Main.java
+++ 단계별로 풀어보기/23.그리디 알고리즘/Problem/src/Main.java
@@ -10,6 +10,38 @@ public class Main {
 
     }
 
+    private static void prob5585() throws IOException {
+        int N = Integer.parseInt(br.readLine());
+
+        int money = 1000 - N;
+        int cnt = 0;
+        while (money > 0) {
+            if (money >= 500) {
+                money -= 500;
+                cnt++;
+            } else if (money >= 100) {
+                money -= 100;
+                cnt++;
+            } else if (money >= 50) {
+                money -= 50;
+                cnt++;
+            } else if (money >= 10) {
+                money -= 10;
+                cnt++;
+            } else if (money >= 5) {
+                money -= 5;
+                cnt++;
+            } else {
+                money -= 1;
+                cnt++;
+            }
+        }
+        sb.append(cnt);
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+
     private static void prob2217() throws IOException {
         int N = Integer.parseInt(br.readLine());
         int arr[] = new int[N];
