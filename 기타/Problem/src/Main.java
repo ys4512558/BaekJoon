43b32a94e54be4c1b8c1f8301f5582827ad3a912
beudicri <ys4512558@naver.com>
2023-12-15 21:48:39 +0900

    SOLVED: PROB2754

--- 기타/Problem/src/Main.java
+++ 기타/Problem/src/Main.java
@@ -13,6 +13,40 @@ public class Main {
 
     }
 
+    private static void prob2754() throws IOException {
+        String str = br.readLine();
+        if (str.equals("A+")) {
+            sb.append(4.3);
+        } else if(str.equals("A0")){
+            sb.append(4.0);
+        } else if (str.equals("A-")) {
+            sb.append(3.7);
+        } else if (str.equals("B+")) {
+            sb.append(3.3);
+        } else if (str.equals("B0")) {
+            sb.append(3.0);
+        } else if (str.equals("B-")) {
+            sb.append(2.7);
+        } else if (str.equals("C+")) {
+            sb.append(2.3);
+        } else if (str.equals("C0")) {
+            sb.append(2.0);
+        } else if (str.equals("C-")) {
+            sb.append(1.7);
+        } else if (str.equals("D+")) {
+            sb.append(1.3);
+        } else if (str.equals("D0")) {
+            sb.append(1.0);
+        } else if(str.equals("D-")){
+            sb.append(0.7);
+        } else {
+            sb.append(0.0);
+        }
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+
     private static void prob10828() throws IOException {
         int N = Integer.parseInt(br.readLine());
         Stack<Integer> stack = new Stack<>();
